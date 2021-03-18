effb5020a7e0a6a26ce0b2376da7330d4e9f0e98
jadtaphon <jadtaphonchinnapan@gmail.com>
2021-03-18 16:24:42 +0700

    set up project game??

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const { Socket } = require("dgram");
+
+const Express = require("express")();
+const Http = require("http").Server(Express);
+const Socketio = require("socket.io")(Http);
+
+var position = {
+    x: 200,
+    y: 200
+}
+
+Socketio.on("connection", socket => {
+    socket.emit("position", position);
+});
+
+Http.listen(3000, () => {
+    console.log("Listening at : 3000...");
+})
